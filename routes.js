13df8453472de01f2315b82b4efb3fd1c820afce
chenzhuonan <chenzhuonan@htmimi.com>
2019-09-29 14:38:23 +0800

    add code

---
+++ routes.js
@@ -0,0 +1,370 @@
+export default {
+    "name": "",
+    "path": "/",
+    "noLoginRedirect": false,
+    "children": [{
+        "name": "about",
+        "path": "/about",
+        "noLoginRedirect": false,
+        "children": [{"name": "about-index", "path": "/about/index", "noLoginRedirect": false}]
+    }, {
+        "name": "decoration",
+        "path": "/decoration",
+        "noLoginRedirect": false,
+        "children": [{
+            "name": "decoration-component",
+            "path": "/decoration/component",
+            "noLoginRedirect": false,
+            "children": [{
+                "name": "decoration-component-DecorationItem",
+                "path": "/decoration/component/DecorationItem",
+                "noLoginRedirect": false
+            }, {
+                "name": "decoration-component-DecorationNav",
+                "path": "/decoration/component/DecorationNav",
+                "noLoginRedirect": false
+            }, {
+                "name": "decoration-component-isLike",
+                "path": "/decoration/component/isLike",
+                "noLoginRedirect": false
+            }, {
+                "name": "decoration-component-Pagination",
+                "path": "/decoration/component/Pagination",
+                "noLoginRedirect": false
+            }]
+        }, {
+            "name": "decoration-details",
+            "path": "/decoration/details",
+            "noLoginRedirect": false,
+            "children": [{
+                "name": "decoration-details-Diary",
+                "path": "/decoration/details/Diary",
+                "noLoginRedirect": false,
+                "children": [{
+                    "name": "decoration-details-Diary-Diary",
+                    "path": "/decoration/details/Diary/:Diary?",
+                    "noLoginRedirect": false
+                }]
+            }, {
+                "name": "decoration-details-FreeDesign",
+                "path": "/decoration/details/FreeDesign",
+                "noLoginRedirect": false
+            }, {
+                "name": "decoration-details-Photographer",
+                "path": "/decoration/details/Photographer",
+                "noLoginRedirect": false,
+                "children": [{
+                    "name": "decoration-details-Photographer-Photographer",
+                    "path": "/decoration/details/Photographer/:Photographer?",
+                    "noLoginRedirect": false
+                }]
+            }, {
+                "name": "decoration-details-Strategy",
+                "path": "/decoration/details/Strategy",
+                "noLoginRedirect": false,
+                "children": [{
+                    "name": "decoration-details-Strategy-Strategy",
+                    "path": "/decoration/details/Strategy/:Strategy?",
+                    "noLoginRedirect": false
+                }]
+            }, {
+                "name": "decoration-details-vedioDetail",
+                "path": "/decoration/details/vedioDetail",
+                "noLoginRedirect": false,
+                "children": [{
+                    "name": "decoration-details-vedioDetail-vedioDetail",
+                    "path": "/decoration/details/vedioDetail/:vedioDetail?",
+                    "noLoginRedirect": false
+                }]
+            }]
+        }, {"name": "decoration-index", "path": "/decoration/index", "noLoginRedirect": false}]
+    }, {"name": "demo", "path": "/demo", "noLoginRedirect": false}, {
+        "name": "drainageTools",
+        "path": "/drainageTools",
+        "noLoginRedirect": false,
+        "children": [{
+            "name": "drainageTools-component",
+            "path": "/drainageTools/component",
+            "noLoginRedirect": false,
+            "children": [{
+                "name": "drainageTools-component-FreeDesign",
+                "path": "/drainageTools/component/FreeDesign",
+                "noLoginRedirect": false
+            }]
+        }, {
+            "name": "drainageTools-index",
+            "path": "/drainageTools/index",
+            "noLoginRedirect": false
+        }, {
+            "name": "drainageTools-quotation",
+            "path": "/drainageTools/quotation",
+            "noLoginRedirect": false,
+            "children": [{
+                "name": "drainageTools-quotation-component",
+                "path": "/drainageTools/quotation/component",
+                "noLoginRedirect": false,
+                "children": [{
+                    "name": "drainageTools-quotation-component-QuotationResult",
+                    "path": "/drainageTools/quotation/component/QuotationResult",
+                    "noLoginRedirect": false
+                }]
+            }, {
+                "name": "drainageTools-quotation-index",
+                "path": "/drainageTools/quotation/index",
+                "noLoginRedirect": false
+            }]
+        }]
+    }, {
+        "name": "findDesign",
+        "path": "/findDesign",
+        "noLoginRedirect": false,
+        "children": [{
+            "name": "findDesign-component",
+            "path": "/findDesign/component",
+            "noLoginRedirect": false,
+            "children": [{
+                "name": "findDesign-component-designer",
+                "path": "/findDesign/component/designer",
+                "noLoginRedirect": false
+            }, {
+                "name": "findDesign-component-findDesignItem",
+                "path": "/findDesign/component/findDesignItem",
+                "noLoginRedirect": false
+            }, {
+                "name": "findDesign-component-FreeDesign",
+                "path": "/findDesign/component/FreeDesign",
+                "noLoginRedirect": false
+            }, {
+                "name": "findDesign-component-isLike",
+                "path": "/findDesign/component/isLike",
+                "noLoginRedirect": false
+            }, {
+                "name": "findDesign-component-Pagination",
+                "path": "/findDesign/component/Pagination",
+                "noLoginRedirect": false
+            }, {
+                "name": "findDesign-component-showOrderLabe",
+                "path": "/findDesign/component/showOrderLabe",
+                "noLoginRedirect": false
+            }]
+        }, {
+            "name": "findDesign-designer",
+            "path": "/findDesign/designer",
+            "noLoginRedirect": false,
+            "children": [{
+                "name": "findDesign-designer-designer",
+                "path": "/findDesign/designer/:designer?",
+                "noLoginRedirect": false
+            }]
+        }, {
+            "name": "findDesign-details",
+            "path": "/findDesign/details",
+            "noLoginRedirect": false,
+            "children": [{
+                "name": "findDesign-details-FreeDesign",
+                "path": "/findDesign/details/FreeDesign",
+                "noLoginRedirect": false
+            }]
+        }, {
+            "name": "findDesign-effectChartDetails",
+            "path": "/findDesign/effectChartDetails",
+            "noLoginRedirect": false,
+            "children": [{
+                "name": "findDesign-effectChartDetails-effectChartDetails",
+                "path": "/findDesign/effectChartDetails/:effectChartDetails?",
+                "noLoginRedirect": false
+            }]
+        }, {
+            "name": "findDesign-findDesignDetails",
+            "path": "/findDesign/findDesignDetails",
+            "noLoginRedirect": false,
+            "children": [{
+                "name": "findDesign-findDesignDetails-findDesignDetails",
+                "path": "/findDesign/findDesignDetails/:findDesignDetails?",
+                "noLoginRedirect": false
+            }]
+        }, {"name": "findDesign-index", "path": "/findDesign/index", "noLoginRedirect": false}]
+    }, {
+        "name": "home",
+        "path": "/home",
+        "noLoginRedirect": false,
+        "children": [{
+            "name": "home-showRoom",
+            "path": "/home/showRoom",
+            "noLoginRedirect": false,
+            "children": [{"name": "home-showRoom-site", "path": "/home/showRoom/:site?", "noLoginRedirect": false}]
+        }, {"name": "home-citySite", "path": "/home/:citySite?", "noLoginRedirect": false}]
+    }, {"name": "index", "path": "/index", "noLoginRedirect": false}, {
+        "name": "login",
+        "path": "/login",
+        "noLoginRedirect": false,
+        "children": [{"name": "login-index", "path": "/login/index", "noLoginRedirect": false}]
+    }, {
+        "name": "mall",
+        "path": "/mall",
+        "noLoginRedirect": false,
+        "children": [{
+            "name": "mall-goodsList",
+            "path": "/mall/goodsList",
+            "noLoginRedirect": false,
+            "children": [{
+                "name": "mall-goodsList-keyword",
+                "path": "/mall/goodsList/:keyword?",
+                "noLoginRedirect": false
+            }]
+        }, {
+            "name": "mall-newProduct",
+            "path": "/mall/newProduct",
+            "noLoginRedirect": false,
+            "children": [{
+                "name": "mall-newProduct-citySite",
+                "path": "/mall/newProduct/:citySite?",
+                "noLoginRedirect": false
+            }]
+        }, {
+            "name": "mall-searchResult",
+            "path": "/mall/searchResult",
+            "noLoginRedirect": false,
+            "children": [{
+                "name": "mall-searchResult-keyword",
+                "path": "/mall/searchResult/:keyword?",
+                "noLoginRedirect": false
+            }]
+        }, {
+            "name": "mall-weeklyHotSale",
+            "path": "/mall/weeklyHotSale",
+            "noLoginRedirect": false,
+            "children": [{
+                "name": "mall-weeklyHotSale-citySite",
+                "path": "/mall/weeklyHotSale/:citySite?",
+                "noLoginRedirect": false
+            }]
+        }, {"name": "mall-citySite", "path": "/mall/:citySite?", "noLoginRedirect": false}]
+    }, {
+        "name": "order",
+        "path": "/order",
+        "noLoginRedirect": false,
+        "children": [{"name": "order-payresult", "path": "/order/payresult", "noLoginRedirect": false}]
+    }, {
+        "name": "personalCenter",
+        "path": "/personalCenter",
+        "noLoginRedirect": false,
+        "children": [{
+            "name": "personalCenter-component",
+            "path": "/personalCenter/component",
+            "noLoginRedirect": false,
+            "children": [{
+                "name": "personalCenter-component-Pagination",
+                "path": "/personalCenter/component/Pagination",
+                "noLoginRedirect": false
+            }, {
+                "name": "personalCenter-component-PersonalItem",
+                "path": "/personalCenter/component/PersonalItem",
+                "noLoginRedirect": false
+            }]
+        }, {
+            "name": "personalCenter-myLike",
+            "path": "/personalCenter/myLike",
+            "noLoginRedirect": false,
+            "children": [{
+                "name": "personalCenter-myLike-component",
+                "path": "/personalCenter/myLike/component",
+                "noLoginRedirect": false,
+                "children": [{
+                    "name": "personalCenter-myLike-component-DecorationItem",
+                    "path": "/personalCenter/myLike/component/DecorationItem",
+                    "noLoginRedirect": false
+                }, {
+                    "name": "personalCenter-myLike-component-Pagination",
+                    "path": "/personalCenter/myLike/component/Pagination",
+                    "noLoginRedirect": false
+                }]
+            }, {
+                "name": "personalCenter-myLike-index",
+                "path": "/personalCenter/myLike/index",
+                "noLoginRedirect": false
+            }]
+        }, {
+            "name": "personalCenter-MyShowHome",
+            "path": "/personalCenter/MyShowHome",
+            "noLoginRedirect": false
+        }, {
+            "name": "personalCenter-PublishShowHome",
+            "path": "/personalCenter/PublishShowHome",
+            "noLoginRedirect": false
+        }]
+    }, {
+        "name": "product",
+        "path": "/product",
+        "noLoginRedirect": false,
+        "children": [{
+            "name": "product-assemble",
+            "path": "/product/assemble",
+            "noLoginRedirect": false,
+            "children": [{
+                "name": "product-assemble-goodsId",
+                "path": "/product/assemble/:goodsId?",
+                "noLoginRedirect": false
+            }]
+        }, {
+            "name": "product-credential",
+            "path": "/product/credential",
+            "noLoginRedirect": false
+        }, {
+            "name": "product-deposit",
+            "path": "/product/deposit",
+            "noLoginRedirect": false,
+            "children": [{
+                "name": "product-deposit-goodsId",
+                "path": "/product/deposit/:goodsId?",
+                "noLoginRedirect": false
+            }]
+        }, {
+            "name": "product-detail",
+            "path": "/product/detail",
+            "noLoginRedirect": false,
+            "children": [{
+                "name": "product-detail-goodsId",
+                "path": "/product/detail/:goodsId?",
+                "noLoginRedirect": false
+            }]
+        }, {
+            "name": "product-introduct",
+            "path": "/product/introduct",
+            "noLoginRedirect": false,
+            "children": [{
+                "name": "product-introduct-parameterId",
+                "path": "/product/introduct/:parameterId?",
+                "noLoginRedirect": false
+            }]
+        }, {
+            "name": "product-package",
+            "path": "/product/package",
+            "noLoginRedirect": false,
+            "children": [{
+                "name": "product-package-goodsId",
+                "path": "/product/package/:goodsId?",
+                "noLoginRedirect": false
+            }]
+        }]
+    }, {
+        "name": "protocol",
+        "path": "/protocol",
+        "noLoginRedirect": false,
+        "children": [{"name": "protocol-index", "path": "/protocol/index", "noLoginRedirect": false}]
+    }, {
+        "name": "quotation",
+        "path": "/quotation",
+        "noLoginRedirect": false,
+        "children": [{
+            "name": "quotation-component",
+            "path": "/quotation/component",
+            "noLoginRedirect": false,
+            "children": [{
+                "name": "quotation-component-QuotationResult",
+                "path": "/quotation/component/QuotationResult",
+                "noLoginRedirect": false
+            }]
+        }, {"name": "quotation-index", "path": "/quotation/index", "noLoginRedirect": false}]
+    }]
+}
